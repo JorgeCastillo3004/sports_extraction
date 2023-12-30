d133f2dbe8a9572e9b5ed59d417a726e4c28a729
jorge <fundest.jcastillo@gmail.com>
2023-12-30 18:32:59 -0400

    Automated commit

--- milestone2.py
+++ milestone2.py
@@ -560,10 +560,10 @@ def main_m2(driver, flag_news = False):
 			
 			dict_ligues_tornaments = find_ligues_torneos(driver)
 			print("League with pin: ", len(dict_ligues_tornaments))
-			dict_leagues_ready = get_dict_results(table= 'league', column = 'league_name, league_id')
+			dict_leagues_ready = get_dict_results(table= 'league', column = 'country_name, league_name, league_id')
 			print("Previous results: ", len(dict_leagues_ready))
 			print(list(dict_leagues_ready.keys()))
-			
+
 			for ligue, ligue_url in dict_ligues_tornaments.items():
 				print(ligue, end = '')
 				wait_update_page(driver, ligue_url, "container__heading")
