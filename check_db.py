5a239cf07e35ae9b49b11c42bc0fd2640c203a9d
jorge <fundest.jcastillo@gmail.com>
2024-01-03 00:39:50 -0400

    Automated commit

--- check_db.py
+++ check_db.py
@@ -8,7 +8,7 @@ from datetime import date, timedelta, datetime
 parser = argparse.ArgumentParser()
 parser.add_argument('--value', type=str, default='zyltsnwfmwakyjuj07169')
 parser.add_argument('--table', type=str, default='league_team')
-parser.add_argument('--column', type=str, default='*')
+parser.add_argument('--column', type=str, default='league_id')
 parser.add_argument('--stadium', type=str, default='lavyynrspzzzlphf08860')
 args = parser.parse_args()
 value = args.value
@@ -30,7 +30,7 @@ con = getdb()
 
 
 league_team , league_id = 'league_team' , 'league_id'
-value = 'zyltsnwfmwakyjuj07169'
+value = 'qnbwpoczwqqaetye99354'
 query = "SELECT {} FROM {} WHERE league_team.league_id ='{}';".format(column , table, value)
 print(query)
 print("delete table league_team")
@@ -55,6 +55,7 @@ print(time)
 
 match_info = {"match_id":random_id(), "match_country":'VENEZUELA',"end_time":time,"match_date":date,\
 			"name":"RIO JANEIRO","place":"RINCONADA","start_time":time,"league_id":'qnbwpoczwqqaetye99354',"stadium_id":'mshhyqazpmavxwck02760'}
+
 save_math_info(match_info)
 
 
