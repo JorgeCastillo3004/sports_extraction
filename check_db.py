384311b9df0440a43cb7b2534d7468ae5e89c0ae
jorge <fundest.jcastillo@gmail.com>
2024-01-02 23:01:56 -0400

    Automated commit

---
+++ check_db.py
@@ -0,0 +1,37 @@
+import psycopg2
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--value', type=str, default='zyltsnwfmwakyjuj07169')
+parser.add_argument('--table', type=str, default='league_team')
+parser.add_argument('--column', type=str, default='*')
+args = parser.parse_args()
+value = args.value
+table = args.table
+column = args.column
+
+
+
+def getdb():
+	return psycopg2.connect(
+			host="localhost",
+			user="wohhu",
+			password="caracas123",
+	dbname='sports_db',
+	)
+
+con = getdb()
+
+
+league_team , league_id = 'league_team' , 'league_id'
+
+query = "SELECT {} FROM {} WHERE legue_id ='{}';".format(column , table, value)
+print("delete table league_team")
+cur = con.cursor()
+cur.execute(query)
+
+results = cur.fetchall()
+
+print("Total results: ", len(results))
+for result in results:
+	print(result)
