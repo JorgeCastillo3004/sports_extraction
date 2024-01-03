c5c3f942de15e3938b7d6d89bb5968da4363b869
jorge <fundest.jcastillo@gmail.com>
2024-01-03 00:55:33 -0400

    Automated commit

--- check_db.py
+++ check_db.py
@@ -8,7 +8,7 @@ from datetime import date, timedelta, datetime
 parser = argparse.ArgumentParser()
 parser.add_argument('--value', type=str, default='zyltsnwfmwakyjuj07169')
 parser.add_argument('--table', type=str, default='league_team')
-parser.add_argument('--column', type=str, default='*')
+parser.add_argument('--column', type=str, default='league_id')
 parser.add_argument('--stadium', type=str, default='lavyynrspzzzlphf08860')
 args = parser.parse_args()
 value = args.value
@@ -36,11 +36,12 @@ print(query)
 cur = con.cursor()
 cur.execute(query)
 
-results = cur.fetchall()
 
-print("Total results: ", len(results))
-for result in results:
-	print(result)
+league_id = cur.fetchone()[0]
+print("league_id: ", league_id)
+# print("Total results: ", len(results))
+# for result in results:
+# 	print(result)
 
 cleaned_text = '15.12. 14:45'#re.findall(r'\d+\.\d+\.\s+\d+\:\d+', date)[0]
 
@@ -53,7 +54,7 @@ print(date)
 print(time)
 
 match_info = {"match_id":random_id(), "match_country":'VENEZUELA',"end_time":time,"match_date":date,\
-			"name":"RIO JANEIRO","place":"RINCONADA","start_time":time,"league_id":'shjltjdkyvldimzo09413',"stadium_id":'mshhyqazpmavxwck02760'}
+			"name":"RIO JANEIRO","place":"RINCONADA","start_time":time,"league_id":league_id, "stadium_id":'mshhyqazpmavxwck02760'}
 
 save_math_info(match_info)
 
