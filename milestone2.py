d27a94420f4e6ed1bfda1dadb2f7a3918a792570
jorge <fundest.jcastillo@gmail.com>
2024-01-02 21:06:21 -0400

    Automated commit

--- milestone2.py
+++ milestone2.py
@@ -190,7 +190,7 @@ def extract_info_results__(driver):
 					id_section_new = get_unique_key(id_section_new, dict_rounds.keys())
 				if count_sub_section != 0:
 					# save current dict
-					stop_validate()
+					# stop_validate()
 					file_name = 'check_points/events/{}/round_{}.json'.format(section_name, current_id_section)
 					save_check_point(file_name, dict_rounds[current_id_section])
 				count_sub_section += 1
@@ -330,14 +330,14 @@ def main_m2(driver, flag_news = False):
 						# save_tournament(dict_tournament) # UNCOMENT
 						league_id = league_info['league_id']
 
-						prin("League_id: ", league_id)
+					prin("League_id: ", league_id)
 
-						list_seasons = get_list_results(league_id , table= 'season', column = 'season_name')						
-						print("list_seasons: ", list_seasons)
-						# list_seasons = []
-						if not(league_info['season_name'] in list_seasons):
-							print("save season info: ", league_info['season_id'])
-							save_season_database(league_info) # UNCOMENT
+					list_seasons = get_list_results(league_id , table= 'season', column = 'season_name')
+					print("list_seasons: ", list_seasons)
+					# list_seasons = []
+					if not(league_info['season_name'] in list_seasons):
+						print("save season info: ", league_info['season_id'])
+						save_season_database(league_info) # UNCOMENT
 					league_check_point[league_name_url] = {'league_name':league_info['league_name'] , 'url':league_url,\
 															 'league_id':league_id, 'season_id':league_info['season_id']}
 
