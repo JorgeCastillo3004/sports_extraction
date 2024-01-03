a835e4a3a9f080d7cd94359a4ed5e9b30c52c35c
jorge <fundest.jcastillo@gmail.com>
2024-01-03 00:09:16 -0400

    Automated commit

--- check_db.py
+++ check_db.py
@@ -5,10 +5,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--value', type=str, default='zyltsnwfmwakyjuj07169')
 parser.add_argument('--table', type=str, default='league_team')
 parser.add_argument('--column', type=str, default='*')
+parser.add_argument('--stadium', type=str, default='lavyynrspzzzlphf08860')
 args = parser.parse_args()
 value = args.value
 table = args.table
 column = args.column
+stadium = args.stadium
 
 
 
@@ -26,6 +28,18 @@ con = getdb()
 league_team , league_id = 'league_team' , 'league_id'
 value = 'zyltsnwfmwakyjuj07169'
 query = "SELECT {} FROM {} WHERE league_team.league_id ='{}';".format(column , table, value)
+print("delete table league_team")
+cur = con.cursor()
+cur.execute(query)
+
+results = cur.fetchall()
+
+print("Total results: ", len(results))
+for result in results:
+	print(result)
+
+print("######### STADIUM RESULTS ############3")
+query = "SELECT {} FROM stadium WHERE stadium.stadium_id ='{}';".format(column, stadium)
 print("delete table league_team")
 cur = con.cursor()
 cur.execute(query)
