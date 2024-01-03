0a541908c7c21608a9581e75c078795b3fd9b2aa
jorge <fundest.jcastillo@gmail.com>
2024-01-03 00:50:19 -0400

    Automated commit

--- check_db.py
+++ check_db.py
@@ -53,7 +53,7 @@ print(date)
 print(time)
 
 match_info = {"match_id":random_id(), "match_country":'VENEZUELA',"end_time":time,"match_date":date,\
-			"name":"RIO JANEIRO","place":"RINCONADA","start_time":time,"league_id":'qnbwpoczwqqaetye99354',"stadium_id":'mshhyqazpmavxwck02760'}
+			"name":"RIO JANEIRO","place":"RINCONADA","start_time":time,"league_id":'shjltjdkyvldimzo09413',"stadium_id":'mshhyqazpmavxwck02760'}
 
 save_math_info(match_info)
 
