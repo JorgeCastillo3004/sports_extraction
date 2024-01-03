3d89e60911f70631f8df7e84e64bf04771e642fe
jorge <fundest.jcastillo@gmail.com>
2024-01-03 15:29:32 -0400

    Automated commit

--- clean_db.py
+++ clean_db.py
@@ -15,6 +15,32 @@ def getdb():
 
 con = getdb()
 
+print("Delete all from score_entity ")
+query = "DELETE FROM score_entity;"
+cur = con.cursor()
+cur.execute(query)
+con.commit()
+
+
+print("Delete all from match_detail ")
+query = "DELETE FROM match_detail;"
+cur = con.cursor()
+cur.execute(query)
+con.commit()
+
+print("Delete all from match ")
+query = "DELETE FROM match;"
+cur = con.cursor()
+cur.execute(query)
+con.commit()
+
+print("Delete all from league_team ")
+query = "DELETE FROM league_team;"
+cur = con.cursor()
+cur.execute(query)
+con.commit()
+
+
 
 print("Delete all from season ")
 query = "DELETE FROM season;"
