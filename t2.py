77c303e198e50d34c48968e638b638d5c0bc3724
jorge <fundest.jcastillo@gmail.com>
2024-01-05 15:55:58 -0400

    Automated commit

--- t2.py
+++ t2.py
@@ -91,7 +91,7 @@ def get_dict_league_ready(sport_id = 'TENNIS'):
 			dict_results[row[0]][row[1]] = {}
 
 		if not row[2] in list(dict_results[row[0]][row[1]].keys() ):
-			dict_results[row[0]][row[1]][row[2]] = {'team_id':row[4]}
+			dict_results[row[0]][row[1]][row[2]] = {'team_id':row[3]}
 
 		# if not row[3] in list(dict_results[row[0]][row[1]][row[2]].keys() ):
 		# 	dict_results[row[0]][row[1]][row[2]][row[3]] = {'team_id':row[4]}
