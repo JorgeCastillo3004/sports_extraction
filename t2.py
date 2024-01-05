5660565713722c0dd9f6567f6d7ab18f2bbac785
jorge <fundest.jcastillo@gmail.com>
2024-01-05 15:24:03 -0400

    Automated commit

--- t2.py
+++ t2.py
@@ -51,6 +51,23 @@ def get_dict_results(table= 'league', column = 'league_name, league_id'):
 	dict_results = {row[0]: row[1] for row in cur.fetchall()}
 	return dict_results
 
+# def get_dict_league_ready(sport_id = 'TENNIS'):
+# 	query = """
+# 		SELECT team.sport_id, team.team_country, league.league_country, team.team_name, team.team_id
+# 		FROM team
+# 		JOIN league_team ON team.team_id = league_team.team_id
+# 		JOIN league ON league_team.league_id = league.league_id
+# 		WHERE team.sport_id = '{}'""".format(sport_id)
+# 	# 
+# 	# -- WHERE team.sport_id = '{}'
+# 	cur = con.cursor()
+# 	cur.execute(query)
+# 	results = cur.fetchall()
+# 	dict_results = {}
+# 	for row in results:
+# 		dict_results.setdefault(row[0], {}).setdefault(row[1], {}).setdefault(row[2], {})[row[3]] = {'team_id': row[4]}	
+
+# 	return dict_results
 def get_dict_league_ready(sport_id = 'TENNIS'):
 	query = """
 		SELECT team.sport_id, team.team_country, league.league_country, team.team_name, team.team_id
@@ -64,14 +81,26 @@ def get_dict_league_ready(sport_id = 'TENNIS'):
 	cur.execute(query)
 	results = cur.fetchall()
 	dict_results = {}
+	# for row in results:
+	# 	dict_results.setdefault(row[0], {}).setdefault(row[1], {}).setdefault(row[2], {})[row[3]] = {'team_id': row[4]}	
 	for row in results:
-		dict_results.setdefault(row[0], {}).setdefault(row[1], {}).setdefault(row[2], {})[row[3]] = {'team_id': row[4]}	
+		if not row[0] in list(dict_results.keys()):
+			dict_results[row[0]] = {}
+
+		if not row[1] in list(dict_results[row[0]].keys() ):
+			dict_results[row[0]][row[1]] = {}
+
+		if not row[2] in list(dict_results[row[0]][row[1]].keys() ):
+			dict_results[row[0]][row[1]][row[2]] = {}
+
+		if not row[3] in list(dict_results[row[0]][row[1]][row[2]].keys() ):
+			dict_results[row[0]][row[1]][row[2]][row[3]] = {'team_id':row[4]}
 
 	return dict_results
 
 con = getdb()
 sport = 'TENNIS'
-dict_sport, results = get_dict_league_ready(sport_id = sport)
+dict_sport = get_dict_league_ready(sport_id = sport)
 
 
 print("dict_sport: ", list(dict_sport.keys()))
