5ef14e80a152f45d630d130c017046636f4f9cb0
jorge <fundest.jcastillo@gmail.com>
2024-01-06 09:50:15 -0400

    Automated commit

--- main_time.py
+++ main_time.py
@@ -1,6 +1,37 @@
-import json
-import schedule
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import chromedriver_autoinstaller
+from selenium import webdriver
+from datetime import datetime
+import pandas as pd
+import random
 import time
+import json
+import re
+import os
+from selenium.webdriver.support.ui import Select
+from datetime import date, timedelta
+import string
+import schedule
+from common_functions import *
+from data_base import *
+from milestone1 import *
+from milestone2 import *
+from milestone3 import *
+from milestone4 import *
+from milestone5 import *
+from milestone6 import *
+
+
+CONFIG = load_json('check_points/CONFIG.json')
+database_enable = CONFIG['DATA_BASE']
+if database_enable:
+    con = getdb()
 
 
 def section1():
