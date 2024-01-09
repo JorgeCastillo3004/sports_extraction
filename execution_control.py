b0eae9b9ae1c74f357726a07195c919fd5f0e5e0
jorge <fundest.jcastillo@gmail.com>
2024-01-09 02:58:38 -0400

    Automated commit

--- execution_control.py
+++ execution_control.py
@@ -22,25 +22,40 @@ days = {
     'saturday': 5,
     'sunday': 6
 }
+
+def extract_news():
+	print("Extracting news: ")
 # with open('input.json', 'r') as file:
 # 	section_schedule = json.load(file)
 count = 0
+day_execution = -1
+execute_ready = False
 while True and count < 100:
 	print(count, end = '-')
+	
 	execution_schedule = section_schedule['main_extract_news']
-	if 'montly' in execution_schedule:
+	if 'montly' in execution_schedule and not execute_ready:
+		print("Case montly: ")
 		interval, day_exe, time_str = execution_schedule.split("|")
-		if datetime.now().day == days['day_exe']:
-
+		if datetime.now().day == days[day_exe]:
 			time_execution = datetime.strptime(time_str, '%H:%M:%S')
 			print(time_execution)
+			execute_ready = True
 
-	if 'weekly' in execution_schedule:
-		interval, day, time_str = execution_schedule.split("|")
+	if 'weekly' in execution_schedule and not execute_ready:
+
+		interval, day_exe, time_str = execution_schedule.split("|")
 		time_execution = datetime.strptime(time_str, '%H:%M:%S')
+		print("Case weekly: ", day_exe, days[day_exe])
 		print(time_execution)
-		if datetime.now() > time_execution:
-			print("Execure function: ")
+		if datetime.now().day == days[day_exe] and datetime.now() > time_execution:		
+			print("Execute function: ")
+			extract_news()
+			execute_ready = True
+			day_execution = datetime.now().day
+	if datetime.now().day != day_execution:
+		execute_ready = False
+		day_execution = -1
 
 	time.sleep(1)
 	count += 1
