f38e1cccb89a8e93e386c1eaaa90a91283010bf2
jorge <fundest.jcastillo@gmail.com>
2024-01-12 01:34:08 -0400

    Automated commit

--- milestone7.py
+++ milestone7.py
@@ -159,7 +159,7 @@ def live_games(driver, list_sports):
 			print("Match id: ", match_id)
 
 			stop_validate()
-			match_id = 'qqos96729'
+			match_id = 'ywse92791'
 			# update_data base
 			# Get score_id home and score_id visitor
 			#{match_detail_id_visitor: False, match_detail_id_home:True}
